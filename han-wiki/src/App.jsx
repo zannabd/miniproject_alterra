da1b78aa5ab755d65d85d64b3dc918808932437b
Fauzan <fabdillah408@gmail.com>
2023-10-24 15:18:06 +0700

    add routing detail

--- han-wiki/src/App.jsx
+++ han-wiki/src/App.jsx
@@ -6,18 +6,22 @@ import JepangPage from "./pages/OtherPage/JepangPage";
 import CreatePage from "./pages/OtherPage/CreatePage";
 import AboutPage from "./pages/OtherPage/AboutPage";
 import OpenAI from "./pages/OtherPage/OpenAIPage";
+import { useState } from "react";
+import DetailPage from "./pages/OtherPage/DetailPage";
 
 function App() {
+  const [movies, setMovies] = useState([])
   return (
     <>
       <Layout>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home movies={movies} setMovies={setMovies}/>} />
           <Route path="/movie/korea" element={<KoreanPage />} />
           <Route path="/movie/jepang" element={<JepangPage/>} />
           <Route path="/create" element={<CreatePage />} />
           <Route path="/openai" element={<OpenAI />} />
           <Route path="/about" element={<AboutPage />} />
+          <Route path="/detail/:id" element={<DetailPage/>}/>
         </Routes>
       </Layout>
     </>
